4a1168b3d1438b57c8a62895f9278e366abba0a2
ar4ibais <ak.hutov@yandex.ru>
2023-10-10 19:08:05 +0300

    made promo section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
 import Header from './components/header/Header'
+import Promo from './components/promo/Promo'
 
 function App() {
 
   return (
     <>
       <Header />
+      <Promo />
     </>
   )
 }
---
+++ src/components/promo/Promo.jsx
@@ -0,0 +1,40 @@
+import './promo.css';
+import promoImg from '../../img/images/header-img.jpg'
+
+const Promo = () => {
+    return (
+        <section className='Promo'>
+            <div className="container">
+                <div className="promo__content">
+                    <div className="promo__text">
+                        <h1 className="promo__title">
+                            <span className='highlight'>
+                                <span>
+                                    LET’S
+                                </span>
+                            </span>
+                            EXPLORE
+                            <span className='highlight highlight--yellow'>
+                                <span>
+                                    UNIQUE
+                                </span>
+                            </span>
+                            CLOTHES.
+                        </h1>
+                        <p className="promo__desc">
+                            Live for Influential and Innovative fashion!
+                        </p>
+                        <a href="/" className="promo__btn">
+                            Show now!
+                        </a>
+                    </div>
+                    <div className="promo__img">
+                        <img src={promoImg} alt="" />
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default Promo;
