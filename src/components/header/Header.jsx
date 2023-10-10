2bf2c8182cb8fc17ab64d9e07f4a5d2659bd3b7a
ar4ibais <ak.hutov@yandex.ru>
2023-10-10 18:35:53 +0300

    made header

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,28 @@
+import './header.css';
+import logoImg from '../../img/icons/logo.svg'
+
+function Header() {
+    return (
+        <header className="header">
+            <div className="container">
+                <div className="header__row">
+                    <a href="/" className="header__logo">
+                        <img src={logoImg} alt="logo" />
+                        <span>FASHION</span>
+                    </a>
+                    <nav className="header__nav">
+                        <ul className="header__list">
+                            <li><a href="/">CATALOGUE</a></li>
+                            <li><a href="/">FASHION</a></li>
+                            <li><a href="/">FAVOURITE</a></li>
+                            <li><a href="/">LIFESTYLE</a></li>
+                            <li><a href="/" className='header__nav-btn'>SIGN UP</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import '../src/styles/common.css'
+import '../src/styles/reset.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
